dbf4c7715d4a5f7d3519703b9b34392783e41cfb
Edson Junior <edson_junior91@hotmail.com>
2020-06-30 00:36:46 -0300

    Exemplo de Modificadores de Acesso com Lampada

---
+++ Exercicio5_1/src/Lampada.java
@@ -0,0 +1,23 @@
+
+public class Lampada {
+	private boolean ligada;
+	
+	public Lampada(boolean ligada) {
+		this.ligada = ligada;
+	}
+	public void  ligar() {
+		ligada = true;
+	}
+	public void desligar() {
+		ligada = false;
+	}
+	
+	public void imprimir() {
+		if(ligada) {
+			System.out.println("Lâmpada Ligada");
+		}else {
+			System.out.println("Lampada Desligada");
+		}
+	}
+	
+}
